9496b7248fdbfc5091e4fae7e294766681383b4f
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-01-24 00:44:53 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-gsap-example';
+  @ViewChild('box', { static: true }) box!: ElementRef<HTMLDivElement>;
+
+  INIT_ANIMATION_FADE_IN = {
+    delay: 0.2,
+    duration: 0.8,
+    opacity: 0,
+    y: -20,
+  };
+
+  ngOnInit(): void {
+    this.initAnimations();
+  }
+
+  initAnimations(): void {
+    gsap.from(this.box.nativeElement, this.INIT_ANIMATION_FADE_IN);
+  }
 }
